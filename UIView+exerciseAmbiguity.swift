f39efc980ed97b7bb4732ac5b75de0d8e914c263
Sergey <zqutui@gmail.com>
2020-12-01 00:30:05 +0200

    Create UIView+exerciseAmbiguity.swift

---
+++ UIView+exerciseAmbiguity.swift
@@ -0,0 +1,19 @@
+extension UIView {
+
+    // From an original idea by Florian Kugler
+    // https://www.objc.io/issues/3-views/advanced-auto-layout-toolbox/
+    
+    class func exerciseAmbiguity(_ view: UIView) {
+        #if DEBUG
+        if view.hasAmbiguousLayout {
+            Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+                view.exerciseAmbiguityInLayout()
+            }
+        } else {
+            for subview in view.subviews {
+                UIView.exerciseAmbiguity(subview)
+            }
+        }
+        #endif
+    }
+}
